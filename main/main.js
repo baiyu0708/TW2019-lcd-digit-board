f4ca082fdf10431799bcb09a88c84e67dd5dde1f
szww33427 <847476048@qq.com>
2019-05-25 20:32:36 +0800

    实现

--- main/main.js
+++ main/main.js
@@ -1,4 +1,84 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+/**
+ * @param number {string} 要显示的数值，如"910"
+ * @returns {string} 以LCD灯的形式显示数值，如：
+ * ._. ... ._.\n
+ * |_| ..| |.|\n
+ * ..| ..| |_|\n
+ */
+module.exports = function main(number) {
+    // 元素[i]存储数字i的显示字符串
+    // 元素[i]是一个数组，含有三个元素，分别为数字i的上、中、下三行的显示字符串
+    let digits = [
+        [
+            "._.",
+            "|.|",
+            "|_|"
+        ], [
+            "...",
+            "..|",
+            "..|"
+        ], [
+            "._.",
+            "._|",
+            "|_."
+        ], [
+            "._.",
+            "._|",
+            "._|"
+        ], [
+            "...",
+            "|_|",
+            "..|"
+        ], [
+            "._.",
+            "|_.",
+            "._|"
+        ], [
+            "._.",
+            "|_.",
+            "|_|"
+        ], [
+            "._.",
+            "..|",
+            "..|"
+        ], [
+            "._.",
+            "|_|",
+            "|_|"
+        ], [
+            "._.",
+            "|_|",
+            "..|"
+        ]
+    ];
+
+    let lineTop = [];
+    let lineMiddle = [];
+    let lineBottom = [];
+
+    for (let digit of digitsOf(number)) {
+        lineTop.push(digits[digit][0]);
+        lineMiddle.push(digits[digit][1]);
+        lineBottom.push(digits[digit][2]);
+    }
+
+    return lineTop.join(" ") + "\n" +
+        lineMiddle.join(" ") + "\n" +
+        lineBottom.join(" ") + "\n";
 };
+
+/**
+ * 返回整数各个数字
+ * @param number {string}
+ * @return {Array}
+ */
+function digitsOf(number) {
+    let digits = [];
+    for (let char of number.split("")) {
+        digits.push(parseInt(char));
+    }
+
+    return digits;
+}
+
+console.log(digitsOf("910"));
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,29 +2,35 @@ const main = require('../main/main');
 
 describe('main()', () => {
 
+    it('should return 0123456789', () => {
+        let actual = main("0123456789");
+        expect(actual).toBe(
+            "._. ... ._. ._. ... ._. ._. ._. ._. ._.\n" +
+            "|.| ..| ._| ._| |_| |_. |_. ..| |_| |_|\n" +
+            "|_| ..| |_. ._| ..| ._| |_| ..| |_| ..|\n");
+    });
+
     it('should return 910', () => {
         let actual = main("910");
         expect(actual).toBe(
-            "._. ... ._.\n"+
-            "|_| ..| |.|\n"+
+            "._. ... ._.\n" +
+            "|_| ..| |.|\n" +
             "..| ..| |_|\n");
     });
 
     it('should return 256', () => {
         let actual = main("256");
         expect(actual).toBe(
-            "._. ._. ._.\n"+
-            "._| |_. |_.\n"+
+            "._. ._. ._.\n" +
+            "._| |_. |_.\n" +
             "|_. ._| |_|\n");
     });
-
 
     it('should return 7', () => {
         let actual = main("7");
         expect(actual).toBe(
-            "._.\n"+
-            "..|\n"+
+            "._.\n" +
+            "..|\n" +
             "..|\n");
     });
-
 });
